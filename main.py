a5ceab5848f05e0efb3b67a2371b7fb053436620
Eric Schneider <esschneider6@gmail.com>
2020-12-13 17:37:01 -0500

    Implementing the placement of the apple

--- main.py
+++ main.py
@@ -37,12 +37,26 @@ class Snake:
         else:
             return direction
 
+class Food:
+    def __init__(self, size):
+        self.width, self.height = size
+        self.edge = self.width // 50
+        self.x_position,self.y_position=self.get_coordinates(player.x_position,player.y_position)
+
+    def get_coordinates(self, player_x, player_y, occupied=True):
+        while occupied:
+            food_x=self.edge*random.randint(0,self.width//50)
+            food_y=self.edge*random.randint(0,self.height//50)
+            occupied=False if food_x!=player_x and food_y!=player_y else True
+        return food_x,food_y
 
 pygame.init()
 size = (900, 600)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Hamiltonian Snake")
+points=1
 player = Snake(size)
+apple=Food(size)
 direction = None
 run = True
 while run:
@@ -54,6 +68,15 @@ while run:
     direction=player.direction_check(direction)
     player.x_position, player.y_position = player.movement(direction)
     screen.fill((0, 0, 0))
+    if player.x_position==apple.x_position and player.y_position==apple.y_position:
+        points+=2
+        apple.x_position,apple.y_position=apple.get_coordinates(player.x_position,player.y_position)
+    pygame.draw.rect(screen, (255, 0, 0), (apple.x_position, apple.y_position, apple.edge, apple.edge))
     pygame.draw.rect(screen, (0, 255, 0), (player.x_position, player.y_position, player.edge, player.edge))
+
+
+
+
     pygame.display.update()
 
+print(points)
