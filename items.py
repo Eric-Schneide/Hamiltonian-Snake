9071e0be4171fc86ceeb5973b9b9eac52a7cb2ee
Eric Schneider <esschneider6@gmail.com>
2021-01-27 12:19:59 -0500

    Implementing editable maps

--- items.py
+++ items.py
@@ -55,30 +55,29 @@ class Snake:
         self.track.pop() if len(self.track) > length else None
         return self.track
 
-    def collision_check(self):
+    def collision_check(self,banned_blocks):
         '''
         Checks if the head of the snake hits a block of the tail, a banned block from editing, or the barrier
         '''
-        return True if self.x_position < 50 or self.x_position > (self.width - 50 - self.edge) or \
+        return self.x_position < 50 or self.x_position > (self.width - 50 - self.edge) or \
                        self.y_position < 50 or self.y_position > (self.height - 50 - self.edge) or \
-                       self.track[0] in self.track[1:] else False
+                       self.track[0] in self.track[1:] or self.track[0] in banned_blocks
 
 class Food:
     '''
     The apple which will increase the score and the length of the snake
     '''
-    def __init__(self, size):
+    def __init__(self, size,track,banned_blocks):
         self.width, self.height = size
         self.edge = (self.width-100) // 50
-        self.x_position, self.y_position = self.edge * random.randint(1, ((self.width-100) // self.edge) - 1)+50, \
-                                           self.edge * random.randint(1, ((self.height-100) // self.edge) - 1)+50
+        self.x_position, self.y_position = self.get_coordinates(track,banned_blocks)
 
-    def get_coordinates(self, track, occupied=True):
+    def get_coordinates(self, track, banned_blocks, occupied=True):
         '''
         finds coordinates for the apple which are not already occupied by the snake after being eaten
         '''
         while occupied:
             food_x = self.edge * random.randint(0, ((self.width-100) // self.edge) - 1)+50
             food_y = self.edge * random.randint(0, ((self.height-100) // self.edge) - 1)+50
-            occupied = False if [food_x, food_y] not in track else True
+            occupied=[food_x,food_y] in track or [food_x,food_y] in banned_blocks
         return food_x, food_y
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from items import Snake, Food, pygame
 import sys
 
-def make_grid(size, edge, screen):
+def make_grid(size, edge, screen,banned_blocks):
     '''
     draws the grid of the board onto the screen
     '''
@@ -10,18 +10,24 @@ def make_grid(size, edge, screen):
         pygame.draw.line(screen, (255, 255, 255), ((i * edge)+50, 50), ((i * edge)+50, height-50))
     for i in range(0, ((height-100) // edge) + 1):
         pygame.draw.line(screen, (255, 255, 255), (50, (i * edge)+50), (width-50, i * edge+50))
+    draw_banned_blocks(size,banned_blocks,screen,edge)
+
+def draw_banned_blocks(size,banned_blocks,screen,edge):
+    for blocks in banned_blocks:
+        pygame.draw.line(screen, (255, 255, 255), (blocks[0], blocks[1]), (blocks[0] + edge, blocks[1] + edge))
 
 def start_menu():
     pass
 
-def maingame(size, screen):
+def maingame(size, screen,banned_blocks):
     clock = pygame.time.Clock()
     length = 1
     max_length = 1600
-    player = Snake(size)
-    apple = Food(size)
-    direction = None
     track = []
+    player = Snake(size)
+    apple = Food(size,track,banned_blocks)
+    direction = None
+
     while True:
         pygame.time.delay(75)
         clock.tick(15)
@@ -36,16 +42,16 @@ def maingame(size, screen):
         if player.x_position == apple.x_position and player.y_position == apple.y_position:
             length += 2
             sys.exit() if length > max_length else None
-            apple.x_position, apple.y_position = apple.get_coordinates(player.track)
+            apple.x_position, apple.y_position = apple.get_coordinates(player.track,banned_blocks)
         pygame.draw.rect(screen, (255, 0, 0), (apple.x_position, apple.y_position, apple.edge, apple.edge))
         for cube in track:
             pygame.draw.rect(screen, (0, 255, 0), (cube[0], cube[1], player.edge, player.edge))
-        if player.collision_check():
+        if player.collision_check(banned_blocks):
             return length,player
-        make_grid(size, player.edge, screen)
+        make_grid(size, player.edge, screen,banned_blocks)
+
 
         pygame.display.update()
-
 
 def loss_screen(length,screen,size,player):
 
@@ -53,7 +59,29 @@ def loss_screen(length,screen,size,player):
     screen.fill((0, 0, 0), (227, 211, 447, 191))
     pygame.display.update()
 
-def mainframe():
+def edit(size, edge, screen, banned_blocks=[]):
+    while True:
+        #screen.fill((0,0,0))
+        make_grid(size,edge,screen,banned_blocks)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                    x,y=pygame.mouse.get_pos()
+                    if x<=50 and y<=50:
+                        return banned_blocks
+                    elif x >= 50 and x <= size[0] - 50 and y>=50 and y<= size[1]-50:
+                        cell_x,cell_y=edge*((x-50)//edge)+50,edge*((y-50)//edge)+50
+                        if [cell_x,cell_y] not in banned_blocks:
+                            pygame.draw.line(screen, (255,255,255), (cell_x,cell_y),(cell_x+edge,cell_y+edge))
+                            banned_blocks.append([cell_x,cell_y])
+                        else:
+                            pygame.draw.line(screen, (0, 0, 0), (cell_x, cell_y), (cell_x + edge, cell_y + edge))
+                            banned_blocks.remove([cell_x, cell_y])
+        pygame.display.update()
+
+def mainframe(banned_blocks=[]):
     '''
     The main game-screen which displays the snake game.
     '''
@@ -63,6 +91,6 @@ def mainframe():
         screen = pygame.display.set_mode(size)
         pygame.display.set_caption("Hamiltonian Snake")
         start_menu()
-        length,player=maingame(size,screen)
-        loss_screen(length,screen,size,player)
+        length,player=maingame(size,screen,banned_blocks)
+        banned_blocks=edit(size,player.edge,screen)
 print(mainframe())
