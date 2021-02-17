356e78e6fb282b0551d891806a397ec22bdaaf81
Eric Schneider <esschneider6@gmail.com>
2021-02-16 21:37:27 -0500

    adding documentation for draw grid

--- drawn_assets.py
+++ drawn_assets.py
@@ -1,17 +1,22 @@
 from colors import colors
 from items import pygame
 
-def make_outline(size,edge,screen,banned_blocks):
-    width,height=size
-    pygame.draw.line(screen,colors.get('white'),(50,50),(50,height-50))
-    pygame.draw.line(screen,colors.get('white'),(width-50,50),(width-50,height-50))
-    pygame.draw.line(screen, colors.get('white'), (50,50), (width-50,50))
-    pygame.draw.line(screen, colors.get('white'), (50,height-50), (width-50,height-50))
-    draw_banned_blocks(banned_blocks,screen,edge)
+
+def make_outline(size, edge, screen, banned_blocks):
+    '''
+    draws an outline of the board onto the screen for playing
+    '''
+    width, height = size
+    pygame.draw.line(screen, colors.get('white'), (50, 50), (50, height - 50))
+    pygame.draw.line(screen, colors.get('white'), (width - 50, 50), (width - 50, height - 50))
+    pygame.draw.line(screen, colors.get('white'), (50, 50), (width - 50, 50))
+    pygame.draw.line(screen, colors.get('white'), (50, height - 50), (width - 50, height - 50))
+    draw_banned_blocks(banned_blocks, screen, edge)
+
 
 def make_grid(size, edge, screen, banned_blocks):
     '''
-    draws the grid of the board onto the screen
+    draws the grid of the board onto the screen for precision in editing
     '''
     width, height = size
     draw_banned_blocks(banned_blocks, screen, edge)
@@ -21,11 +26,9 @@ def make_grid(size, edge, screen, banned_blocks):
         pygame.draw.line(screen, colors.get('white'), (50, (i * edge) + 50), (width - 50, i * edge + 50))
 
 
-
 def draw_banned_blocks(banned_blocks, screen, edge):
     '''
     draws lines onto the grid through the banned blocks to mark them
     '''
     for blocks in banned_blocks:
-        pygame.draw.rect(screen, colors.get('purple'), (blocks[0], blocks[1],edge,edge))
-
+        pygame.draw.rect(screen, colors.get('purple'), (blocks[0], blocks[1], edge, edge))
