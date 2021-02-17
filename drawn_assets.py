1709525d0de53fd0eb10faf9de1f23755405cd89
Eric Schneider <esschneider6@gmail.com>
2021-02-16 21:31:43 -0500

    changed banned block marker to a purple square to match the grid-less board

--- drawn_assets.py
+++ drawn_assets.py
@@ -14,11 +14,12 @@ def make_grid(size, edge, screen, banned_blocks):
     draws the grid of the board onto the screen
     '''
     width, height = size
+    draw_banned_blocks(banned_blocks, screen, edge)
     for i in range(0, ((width - 100) // edge) + 1):
         pygame.draw.line(screen, colors.get('white'), ((i * edge) + 50, 50), ((i * edge) + 50, height - 50))
     for i in range(0, ((height - 100) // edge) + 1):
         pygame.draw.line(screen, colors.get('white'), (50, (i * edge) + 50), (width - 50, i * edge + 50))
-    draw_banned_blocks(banned_blocks, screen, edge)
+
 
 
 def draw_banned_blocks(banned_blocks, screen, edge):
@@ -26,5 +27,5 @@ def draw_banned_blocks(banned_blocks, screen, edge):
     draws lines onto the grid through the banned blocks to mark them
     '''
     for blocks in banned_blocks:
-        pygame.draw.line(screen, colors.get('white'), (blocks[0], blocks[1]), (blocks[0] + edge, blocks[1] + edge))
+        pygame.draw.rect(screen, colors.get('purple'), (blocks[0], blocks[1],edge,edge))
 
