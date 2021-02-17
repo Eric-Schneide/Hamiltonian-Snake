d7ef81fa05a67b642a4b3d0eb2adf133594e9832
Eric Schneider <esschneider6@gmail.com>
2021-02-16 21:24:39 -0500

    adding simple outline as a replacement for entire grid

---
+++ drawn_assets.py
@@ -0,0 +1,30 @@
+from colors import colors
+from items import pygame
+
+def make_outline(size,edge,screen,banned_blocks):
+    width,height=size
+    pygame.draw.line(screen,colors.get('white'),(50,50),(50,height-50))
+    pygame.draw.line(screen,colors.get('white'),(width-50,50),(width-50,height-50))
+    pygame.draw.line(screen, colors.get('white'), (50,50), (width-50,50))
+    pygame.draw.line(screen, colors.get('white'), (50,height-50), (width-50,height-50))
+    draw_banned_blocks(banned_blocks,screen,edge)
+
+def make_grid(size, edge, screen, banned_blocks):
+    '''
+    draws the grid of the board onto the screen
+    '''
+    width, height = size
+    for i in range(0, ((width - 100) // edge) + 1):
+        pygame.draw.line(screen, colors.get('white'), ((i * edge) + 50, 50), ((i * edge) + 50, height - 50))
+    for i in range(0, ((height - 100) // edge) + 1):
+        pygame.draw.line(screen, colors.get('white'), (50, (i * edge) + 50), (width - 50, i * edge + 50))
+    draw_banned_blocks(banned_blocks, screen, edge)
+
+
+def draw_banned_blocks(banned_blocks, screen, edge):
+    '''
+    draws lines onto the grid through the banned blocks to mark them
+    '''
+    for blocks in banned_blocks:
+        pygame.draw.line(screen, colors.get('white'), (blocks[0], blocks[1]), (blocks[0] + edge, blocks[1] + edge))
+
