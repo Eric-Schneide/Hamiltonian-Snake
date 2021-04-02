8a612949c52336af17f56301e55b77d48fb51211
Eric Schneider <esschneider6@gmail.com>
2021-04-01 20:32:18 -0400

    added draw back arrow method

--- drawn_assets.py
+++ drawn_assets.py
@@ -33,3 +33,11 @@ def draw_banned_blocks(banned_blocks, screen, edge):
     for blocks in banned_blocks:
         pygame.draw.rect(screen, colors.get('dark_purple'), (blocks[0], blocks[1], edge, edge))
         pygame.draw.rect(screen,colors.get('purple'),(blocks[0]+1,blocks[1]+1,edge-2,edge-2))
+
+def draw_arrow(screen):
+    '''
+    draws back arrow
+    '''
+    pygame.draw.line(screen, colors.get('white'), (10, 25), (45, 25))
+    pygame.draw.line(screen, colors.get('white'), (10, 25), (10 + 20 * (3 ** (1 / 2) / 2), (25 - 20 / 2)))
+    pygame.draw.line(screen, colors.get('white'), (10, 25), (10 + 20 * (3 ** (1 / 2) / 2), (25 + 20 / 2)))
