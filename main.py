f74689449c5827829c395ded933034ff3d267c89
Eric Schneider <esschneider6@gmail.com>
2021-04-06 12:35:30 -0400

    optimizing settings to save the previous game's selected mode

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from game import maingame, pygame
-from screens import start_menu, loss_screen,settings
+from screens import start_menu, settings, loss_screen
+from items import Snake
 
 
-def mainframe(banned_blocks=[]):
+def mainframe(banned_blocks=[],gamemode=[0,0,0], menu_skip=False, settings_skip=False):
     '''
     The main game-screen which displays the snake game.
     '''
@@ -13,10 +14,12 @@ def mainframe(banned_blocks=[]):
         screen = pygame.display.set_mode(size)
         pygame.display.set_caption("Hamiltonian Snake")
         base_font = pygame.font.SysFont('', 48)
-        start_menu()
-        settings(size,screen,base_font)
-        length, player = maingame(size, screen, base_font, banned_blocks)
-        banned_blocks = loss_screen(length, screen, size, base_font, player, banned_blocks)
+        player = Snake(size)
+        start_menu() if not menu_skip else None
+        if not settings_skip:
+            menu_skip, banned_blocks, gamemode = settings(size, screen, base_font, player,gamemode, banned_blocks)
+        length = maingame(size, screen, base_font, player, banned_blocks, gamemode)
+        settings_skip = loss_screen(length, screen, size, base_font, player, banned_blocks)
 
 
 mainframe()
