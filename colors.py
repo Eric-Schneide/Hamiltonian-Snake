8c26509533b0700b8676db6cf031407ec808e215
Eric Schneider <esschneider6@gmail.com>
2021-04-18 15:59:41 -0400

    reformatting for readability

--- colors.py
+++ colors.py
@@ -5,11 +5,11 @@ colors = {
     "white": (255, 255, 255),
     "black": (0, 0, 0),
     "purple": (100, 50, 140),
-    "blue":(0,0,255),
-    "orange":(255,100,0),
-    "dark_purple":(70,20,110),
-    "dark_green": (0,150,0),
-    "dark_red":(150,0,0),
-    "dark_blue":(0,0,100),
-    "dark_orange":(150,60,0)
+    "blue": (0, 0, 255),
+    "orange": (255, 100, 0),
+    "dark_purple": (70, 20, 110),
+    "dark_green": (0, 150, 0),
+    "dark_red": (150, 0, 0),
+    "dark_blue": (0, 0, 100),
+    "dark_orange": (150, 60, 0)
 }
